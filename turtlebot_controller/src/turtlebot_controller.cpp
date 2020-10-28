50850b7e151d62334ffd287d9d46f95fe70bc06d
Carmine Recchiuto <carmine.recchiuto@dibris.unige.it>
2020-10-28 15:12:55 +0000

    First commit

---
+++ turtlebot_controller/src/turtlebot_controller.cpp
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+#include "turtlesim/Pose.h"
+#include "geometry_msgs/Twist.h"
+
+ros::Publisher pub;
+
+void positionCallback(const turtlesim::Pose::ConstPtr& msg)
+{
+  ROS_INFO("Robot position [%f, %f]", msg->x, msg->y);
+  geometry_msgs::Twist vel;
+  vel.linear.x=0.1;
+  vel.angular.z=0.1;
+  pub.publish(vel);
+}
+
+int main(int argc, char **argv)
+{
+   ros::init(argc, argv, "turtlebot_controller");
+   ros::NodeHandle n;
+
+   pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+   ros::Subscriber sub = n.subscribe("/turtle1/pose", 1000, positionCallback);
+
+   ros::spin();
+
+   return 0;
+}
